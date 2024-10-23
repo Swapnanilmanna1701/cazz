744f6f8f60093bc193b8ac7d1685c7359162d2f3
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-10-23 23:00:44 +0530

    packages installation and integrate properly

--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -1,10 +1,9 @@
-
 import { neon } from "@neondatabase/serverless";
 
 import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL as string);
+const sql = neon(process.env.DATABASE_URL);
 
 export const db = drizzle(sql, { schema });
