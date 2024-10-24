61ae31e2cedbad0c0feb20ce0c2d2d3b2b04552f
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-10-25 02:31:05 +0530

    another page added

--- app/dashboard/chatbot/page.tsx
+++ app/dashboard/chatbot/page.tsx
@@ -1,9 +1,13 @@
 import Chatbox from "@/components/Chatbot";
 import React from "react";
+import { Navbar } from "@/components/Navbar";
 
 const page = () => {
   return (
     <div>
+      <div className="bg-black text-gray-100 h-20">
+        <Navbar />
+      </div>
       <Chatbox />
     </div>
   );
