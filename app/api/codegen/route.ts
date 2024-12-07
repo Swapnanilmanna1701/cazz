35a1e821f500328d6798dc4fe03c7b649e630488
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-12-07 23:26:09 +0530

    error

--- app/api/codegen/route.ts
+++ app/api/codegen/route.ts
@@ -1,19 +1,22 @@
+import { google } from '@ai-sdk/google';
+
 //import { google } from "@ai-sdk/google";
-import { GoogleGenerativeAI } from '@google/generative-ai';
-const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY as string);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 import { streamObject } from "ai";
 import { z } from "zod";
 export const dynamic = "force-dynamic";
 export const maxDuration = 30;
+const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY as string);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
 
 
 export async function POST(req: Request) {
   const context = await req.json();
 
   const result = await streamObject({
-    model: google("gemini-1.5-pro"),
+    model: google("gemini-1.5-flash"),
     system:
       "You are the best coder and code generator. you take the code and give the converted code in the perfect format with some bit of explanations. you will reject everything except codes.",
     prompt: context,
@@ -26,16 +29,6 @@ export async function POST(req: Request) {
 
   return result.toTextStreamResponse();
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
