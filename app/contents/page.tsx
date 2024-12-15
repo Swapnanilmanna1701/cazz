ed2d28e8cde0ca1de0f8f7c3d583b0842256020b
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-12-16 00:13:04 +0530

    some other component added

--- app/contents/page.tsx
+++ app/contents/page.tsx
@@ -1,24 +1,35 @@
-"use client"
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
+import { Cover } from "@/components/ui/cover";
+
 import { Navbar } from "@/components/Navbar";
-import SearchSection from '@/components/content/SearchSection'
-import TemplateListSection from '@/components/content/TemplateListSection'
+import SearchSection from "@/components/content/SearchSection";
+import TemplateListSection from "@/components/content/TemplateListSection";
 
 function Dashboard() {
-  const [userSearchInput,setUserSearchInput]=useState<string>()
+  const [userSearchInput, setUserSearchInput] = useState<string>();
   return (
-    
     <div>
-        <div className="bg-black text-gray-100 h-20">
-        <Navbar/>
-    </div>
-        {/* Search Section  */}
-        <SearchSection onSearchInput={(value:string)=>setUserSearchInput(value)} />
+      <div className="bg-black text-gray-100 h-20">
+        <Navbar />
+      </div>
+      <div>
+      <h1 className="md:text-6xl text-2xl text-center md:mt-12 font-bold">
+            Create All Types of Content <br /> With{" "}
+            <Cover>
+              <span className=" dark:text-neutral-100">Lightning Speed</span>
+            </Cover>
+          </h1>
+      </div>
+      {/* Search Section  */}
+      <SearchSection
+        onSearchInput={(value: string) => setUserSearchInput(value)}
+      />
 
-        {/* Template List Section  */}
-        <TemplateListSection userSearchInput={userSearchInput} />
+      {/* Template List Section  */}
+      <TemplateListSection userSearchInput={userSearchInput} />
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
