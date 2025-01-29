46c4186a7aa7d48d74374f128e58806a78a51f86
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2025-01-30 01:39:47 +0530

    font styling of  content page

--- components/content/SearchSection.tsx
+++ components/content/SearchSection.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { Particles } from "@/components/particles";
+import VariableFontHoverByRandomLetter from "@/components/letterhover";
 
 function SearchSection({ onSearchInput }: any) {
   const { resolvedTheme } = useTheme();
@@ -14,7 +15,17 @@ function SearchSection({ onSearchInput }: any) {
   return (
     <div className="relative p-10 m-5 border flex flex-col justify-center items-center text-white overflow-hidden rounded-lg  bg-black md:shadow-xl">
       <span className="pointer-events-none z-10 whitespace-pre-wrap text-center font-semibold leading-none">
-      <h2 className="text-3xl font-bold">Browse All Services We Provided</h2>
+      <div className="pointer-events-auto w-full h-full rounded-lg items-center justify-center font-overusedGrotesk p-5 bg-gradient-to-br text-teal bg-none ">
+      <div className="w-full h-full items-center justify-center flex">
+        <VariableFontHoverByRandomLetter
+          label="Browse all the contents!"
+          staggerDuration={0.03}
+          className="rounded-full items-center flex justify-center cursor-pointer px-8 py-2 bg-clip-text text-transparent bg-gradient-to-r from-pink-600 via-purple-500 to-cyan-300 align-text-top text-4xl sm:text-5xl md:text-7xl"
+          fromFontVariationSettings="'wght' 500, 'slnt' 0"
+          toFontVariationSettings="'wght' 800, 'slnt' 0"
+        />
+      </div>
+    </div>
       <p className="text-3xl my-5">What would you like to create today?</p>
       <div className="w-full flex justify-center pointer-events-auto">
         <div className="flex gap-2 items-center p-2 border rounded-3xl bg-white border-blue-700 my-5 w-[70%] pointer-events-auto">
